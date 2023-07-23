5afc95b619ad619b143fee6e7b798bac0e591b18
Asalah3 <asalahsayed3@gmail.com>
2023-07-23 20:28:15 +0300

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by Asalah Sayed on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
